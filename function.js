545b4db99e8095445d4a80719df1e99fcb35a8e1
KiTORS <imkim@imkim.dk>
2017-03-19 19:01:06 +0100

    rutevejledning prøve

--- function.js
+++ function.js
@@ -4,6 +4,11 @@ var list;
 var list_groups;
 var company_clicked = false;
 var currentpost = 0;
+var question_window_open = true;
+var questionboard = false;
+var question_answer = false;
+var directionsService;
+var directionsDisplay;
 
 // -- GOOGLE MAP -- //
 function initMap() {
@@ -16,6 +21,9 @@ function initMap() {
         },
         disableDefaultUI: true,
     });
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(map);
 
     // -- GEOLOCATION - MY LOCATION -- //
     if (navigator.geolocation) {
@@ -32,17 +40,20 @@ function initMap() {
         });
         console.log("my location loadet")
     }
+    $.getJSON("data.json", showList);
+    $.getJSON("data_groups.json", showList_groups);
 }
 
 // -- JSON DATA -- //
-$.getJSON("data.json", showList);
-$.getJSON("data_groups.json", showList_groups);
 
 function showList(data) {
     console.log("JSON Loadet");
     data.forEach(createMarker);
     list = data;
-    console.log(data[1].id);
+    list.forEach(each => {
+        each.position = new google.maps.LatLng(each.position.lat, each.position.lng);
+    });
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
 }
 
 function showList_groups(data) {
@@ -84,21 +95,39 @@ $(".score_button").on("click", score_toggle);
 $(".question_button").on("click", question_toggle);
 
 function score_toggle() {
-    console.log("score knap togglet")
+    console.log("score knap togglet");
     $(".scoreboard").toggleClass("scoretoggle");
     score_group();
 }
 
 function question_toggle() {
-    console.log("question knap togglet")
-    $(".questionboard").toggleClass("questiontoggle");
-    questions();
+    console.log("question knap klikket på")
+    if ((questionboard == true) && (question_answer == false)) {
+        console.log("fjerner questionboard fordi questionboard=", questionboard, "question_answer=", question_answer)
+        $(".questionboard").removeClass("question_open");
+        $(".questionboard").on("click", question_answer_open);
+        questionboard = false;
+        console.log("questionboard bliver sat til", questionboard);
+    } else
+    if ((questionboard == false) && (question_answer == false)) {
+        console.log("åbner questionboard fordi questionboard=", questionboard, "question_answer=", question_answer)
+        $(".questionboard").addClass("question_open");
+        questionboard = true;
+        console.log("questionboard bliver sat til", questionboard);
+        questions();
+    } else
+    if ((questionboard == false) && (question_answer == true)) {
+        console.log("fjerner question answer fordi question_answer=", question_answer, "questionboard=", questionboard)
+        $(".question_answer_window").css("display", "none");
+        $(".questionboard").removeClass("question_open");
+        question_answer = false;
+        console.log("question_answer bliver sat til", question_answer);
+    };
 }
 
 // -- SCORE -- //
 
 function score_group(e) {
-
     console.log("click on score");
 
     // -- KLONER RAMMER -- //
@@ -127,8 +156,8 @@ function score_group(e) {
 }
 
 function score_company(e) {
-
     console.log("click on score");
+
     // -- KLONER RAMMER -- //
     var windowklon = document.querySelector("#score_window").content.cloneNode(true);
     list_groups.forEach(scoreclone_company);
@@ -136,6 +165,7 @@ function score_company(e) {
     // -- KLONER INDHOLD -- //
     function scoreclone_company(e) {
         console.log("kloner company score");
+
         // -- CLONE -- //
         var klon = windowklon.querySelector("#score_row").content.cloneNode(true);
 
@@ -157,16 +187,18 @@ function score_company(e) {
 
 function questions(e) {
 
-    console.log("klikket på spørgsmålsknap");
+    console.log("viser spørgsmål");
 
     // -- KLONER RAMMER -- //
     var windowklon = document.querySelector("#question_window").content.cloneNode(true);
+    console.log("windowklon er defineret");
     list.forEach(questionclone);
     currentpost = list[0].id;
-
     // -- KLONER INDHOLD -- //
     function questionclone(postdata) {
+        console.log("kloner før if");
         if (postdata.id == currentpost) {
+            test = postdata;
             console.log("kloner spørgsmål");
             var klon = windowklon.querySelector("#question").content.cloneNode(true);
 
@@ -178,10 +210,47 @@ function questions(e) {
             klon.querySelector(".data_a4").textContent = postdata.a4;
 
             windowklon.querySelector(".window").appendChild(klon);
-        } else {}
-    }
 
-    // -- INDSÆTTER INDHOLD I DIV -- //
-    document.querySelector(".questionboard").innerHTML = "";
-    document.querySelector(".questionboard").appendChild(windowklon);
+            // -- INDSÆTTER INDHOLD I DIV -- //
+            document.querySelector(".questionboard").innerHTML = "";
+            document.querySelector(".questionboard").appendChild(windowklon);
+        };
+    };
+}
+
+// -- TAK FOR SVAR PÅ SPØRGSMÅL -- //
+
+function question_answer_open() {
+    if (question_answer == false) {
+        console.log("question answer åbner fordi den er", question_answer, "og questionboard=", questionboard)
+        $(".question_answer_window").css("display", "block");
+        $(".questionboard").removeClass("question_open");
+        questionboard = false;
+        question_answer = true;
+        console.log("question answer bliver sat til", question_answer, "og questionboard bliver sat til", questionboard);
+    } else {
+        console.log("gør ingenting da question answer er åben")
+    }
+}
+
+// -- DIRECTIONS / RUTEVEJLEDNING -- //
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+    directionsService.route({
+        origin: list[0].position,
+        destination: list[1].position,
+
+
+        optimizeWaypoints: true,
+        travelMode: 'WALKING'
+    }, function (response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            //  var route = response.routes[0];
+
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
 }
