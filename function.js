d3cca598e5bd7eb3a7a36dd73e4d303ae6a06ca5
KiTORS <imkim@imkim.dk>
2017-03-21 16:31:09 +0100

    HVAD VIRKER: rutevejledning til næste marker, markers med skiftende farve, spørgsmål og score button, spørgsmål kommer op når du går fra posten, gamle gps markers bliver slettet

--- function.js
+++ function.js
@@ -9,87 +9,384 @@ var questionboard = false;
 var question_answer = false;
 var directionsService;
 var directionsDisplay;
+var info_open = false;
+var myPos;
+var gpsmarker = [];
+var currentMarker;
+var newMarker;
+var newMarkerPosition;
+var currentMarkerPosition;
+var closestmarker;
+var pastMarker;
+var markerNumber = 0;
+var silverStyle = [
+    {
+        "featureType": "all",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#cbdae2"
+            }
+        ]
+    }, {
+        "featureType": "transit.station.rail",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ff0000"
+            },
+            {
+                "weight": "1"
+            }
+        ]
+    }, {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "1"
+            }
+        ]
+    }, {
+        "featureType": "poi.sports_complex",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    }, {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            },
+            {
+                "lightness": "30"
+            }
+        ]
+    }, {
+        "featureType": "poi.medical",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    }, {
+        "featureType": "poi.government",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    }, {
+        "featureType": "poi.attraction",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "color": "#ff0000"
+            }
+        ]
+    }, {
+        "featureType": "landscape.natural.landcover",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d7adad"
+            }
+        ]
+    }, {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    }
+];
 
 // -- GOOGLE MAP -- //
 function initMap() {
-    console.log("Google Map Loadet");
+
+    // -- LOAD RUTEVEJLEDNING SERVICE -- //
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+        suppressMarkers: true
+    });
+    console.log("Rutevejledning Service loadet")
+
+    // -- LOAD GOOGLE MAP -- //
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
+        styles: silverStyle,
         center: {
             lat: 55.706510,
             lng: 12.539087
         },
         disableDefaultUI: true,
     });
-    directionsService = new google.maps.DirectionsService;
-    directionsDisplay = new google.maps.DirectionsRenderer;
+    console.log("Google Map Loadet");
+
+    // -- SÆT RUTEVEJLEDNING PÅ MAP -- //
     directionsDisplay.setMap(map);
+    console.log("Rutevejledning forbundet til map")
 
-    // -- GEOLOCATION - MY LOCATION -- //
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(function (position) {
-            var minPos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            map.setCenter(minPos);
-            var mig = new google.maps.Marker({
-                position: minPos,
-                map: map,
-            });
-        });
-        console.log("my location loadet")
-    }
-    $.getJSON("data.json", showList);
+    //    // --SVG OVERLAY-- //
+    //    var bounds = {
+    //        north: 55.69575753907373,
+    //        south: 55.67674229610198,
+    //        east: 12.654576214658732,
+    //        west: 12.563681515562052
+    //
+    //    }
+    //    var overlay = new google.maps.GroundOverlay('img/svgkort.svg', bounds);
+    //    overlay.setMap(map);
+
+    // -- LOAD JSON DATA -- //
     $.getJSON("data_groups.json", showList_groups);
+    $.getJSON("data.json", showList);
+
+    // -- JSON GRUPPE DATA -- //
+    function showList_groups(data) {
+        list_groups = data;
+        console.log("Gruppe data loadet");
+    }
+
+    // -- JSON POST DATA -- //
+    function showList(data) {
+        list = data;
+        console.log("Post data loadet");
+        console.log("Min position er igang med at blive fundet...");
+    }
+
+    // -- FIND MIN POSITION -- //
+    var watchID = navigator.geolocation.watchPosition(function (position) {
+        myPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        console.log("Min position er fundet og sat til", position.coords.latitude, position.coords.longitude);
+
+        // -- SLETTER MIN GAMLE POSITION -- //
+        removeMyLastPosition();
+
+        function removeMyLastPosition() {
+            console.log("sletter min gamle position");
+            for (i = 0; i < gpsmarker.length; i++) {
+                gpsmarker[i].setMap(null);
+            }
+        }
+
+        // -- INDSÆTTER MIN POSITION PÅ MAPPET -- //
+        map.setCenter(myPos);
+        var me = new google.maps.Marker({
+            position: myPos,
+            map: map,
+        });
+        gpsmarker.push(me);
+
+        console.log("GPS marker er sat på min position");
+        findDistance();
+
+        // -- TJEK DISTANCEN IMELLEM MIG MARKER -- //
+        function findDistance() {
+
+            // -- FIND AFSTAND PÅ NUVÆRENDE MARKER OG MIG -- //
+            currentMarker = list[markerNumber];
+            currentMarkerPosition = new google.maps.LatLng(currentMarker.position.lat, currentMarker.position.lng);
+            var currentMarkerDistance = google.maps.geometry.spherical.computeDistanceBetween(myPos, currentMarkerPosition);
+            console.log("tjekker hvor langt du er fra currentMarker", markerNumber, currentMarkerDistance);
+
+            // -- FIND AFSTAND PÅ NÆSTE MARKER OG MIG -- //
+            nextMarker = list[markerNumber + 1];
+            nextMarkerPosition = new google.maps.LatLng(nextMarker.position.lat, nextMarker.position.lng);
+            var nextMarkerDistance = google.maps.geometry.spherical.computeDistanceBetween(myPos, nextMarkerPosition);
+            console.log("tjekker hvor langt du er fra nextMarker", markerNumber + 1, nextMarkerDistance);
+
+            // -- DEN FØRSTE MARKER -- //
+            if (markerNumber == 0) {
+                console.log("Da du er på starten af turen, er currentMarker sat til post 1(START)");
+                markerNumber++;
+                applynewmarker();
+            } else
+            if ((currentMarkerDistance < 20) && (nextMarkerDistance > 20)) {
+                console.log("Du er længere væk end 20 meter fra din currentMarker, så nu bliver nextMarker vist");
+                markerNumber++;
+                console.log("marker number er sat til", markerNumber);
+                applynewmarker();
+
+                $(".question_button").css("display", "block");
+                console.log("Spørgsmålet til sidste stop er nu klar")
+            }
+        }
+    })
+
+    // -- FIND NÆSTE MARKER  -- //
+    function applynewmarker() {
+        var customMarker;
+
+        if (currentMarker.id == list[0].id) {
+            console.log("den første marker er sat og er grøn");
+            customMarker = {
+                url: 'img/kort_pin_past.svg',
+            };
+            firstMarkerPosition = new google.maps.LatLng(list[0].position.lat, list[0].position.lng);
+            firstMarker = new google.maps.Marker({
+                position: firstMarkerPosition,
+                map: map,
+                title: list[0].name,
+                icon: customMarker,
+            });
+
+        } else
+        if (currentMarker.id == list[markerNumber - 1].id) {
+            console.log("Alle past markers er blevet skiftet til grønne");
+            customMarker = {
+                url: 'img/kort_pin_past.svg',
+            };
+            oldMarkerPosition = new google.maps.LatLng(list[markerNumber - 1].position.lat, list[markerNumber - 1].position.lng);
+            oldMarker = new google.maps.Marker({
+                position: oldMarkerPosition,
+                map: map,
+                title: list[markerNumber].name,
+                icon: customMarker,
+            });
+        }
+        if (nextMarker.id == list[markerNumber].id) {
+            console.log("Den næste marker er blevet sat og er rød")
+            customMarker = {
+                url: 'img/kort_pin_next.svg',
+            };
+            newMarkerPosition = new google.maps.LatLng(list[markerNumber].position.lat, list[markerNumber].position.lng);
+            newMarker = new google.maps.Marker({
+                position: newMarkerPosition,
+                map: map,
+                title: list[markerNumber].name,
+                icon: customMarker,
+            });
+            newMarker.addListener("click", clickOnMarker);
+            makeRoute();
+        }
+
+        // -- TRIGGER RUTE -- //
+
+        // -- GØR KLAR TIL NÆSTE MARKER -- //
+
+        // -- TRIGGER MARKER KLIK -- //
+
+    };
+
+
+
+    // -- RUTEVEJLEDNING -- //
+    function makeRoute() {
+
+        // -- DEFINERER RUTEN -- //
+        var request = {
+            origin: myPos,
+            destination: newMarkerPosition,
+            travelMode: google.maps.DirectionsTravelMode.WALKING
+        };
+
+        // -- SÆTTER RUTEVEJLEDNING IGANG -- //
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                console.log("rutevejledning er startet");
+            }
+        });
+    }
 }
 
-// -- JSON DATA -- //
+// -- CLICK ON MARKER -- //
+function clickOnMarker(marker) {
+    console.log("Du har trykket på en marker");
+    var infowindow = new google.maps.InfoWindow({});
 
-function showList(data) {
-    console.log("JSON Loadet");
-    data.forEach(createMarker);
-    list = data;
-    list.forEach(each => {
-        each.position = new google.maps.LatLng(each.position.lat, each.position.lng);
-    });
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-}
+    // -- HVIS INFOWINDOW IKKE ER ÅBENT -- //
+    if (info_open == false) {
 
-function showList_groups(data) {
-    console.log("JSON groups Loadet");
-    list_groups = data;
-}
-
-// -- LAV EN MARKER -- //
-function createMarker(post) {
-
-    // -- MARKER VARIABEL --//
-    console.log("Marker Made");
-    var marker = new google.maps.Marker({
-        position: post.position,
-        map: map,
-        title: post.name
-    });
-    marker.addListener("click", clickOnMarker);
-
-
-    // -- CLICK ON MARKER -- //
-    function clickOnMarker() {
-        console.log("click on marker");
-        var infowindow = new google.maps.InfoWindow({});
-
-        // -- CLONE -- //
+        // -- KLON -- //
         var klon = document.querySelector("#infobox").content.cloneNode(true);
 
-        klon.querySelector(".data_name").textContent = post.name;
-        klon.querySelector(".data_about").textContent = post.about;
+        klon.querySelector(".data_name").textContent = marker.title;
+        klon.querySelector(".data_about").textContent = marker.about;
 
+        // -- INDSÆT KLON I HTML -- //
         infowindow.setContent(klon);
-        infowindow.open(map, marker);
-
+        infowindow.open(map, newMarker);
+        info_open = true;
     }
+
+    // -- HVIS INFOWINDOW ER ÅBENT -- //
+    else {
+        info_open = false;
+    };
+
 }
+
+
+
+// 2. indsæt rigtigt design:
+//// -- Marker
+//// -- gps marker
+//// -- spørgsmål ikon
+//// -- questionboard
+//// -- scoreboard
+//// -- Start screen
+//// -- Slut screen
+// 3. Rigtig spørgsmål til rigtig post
+// 4. Optimér kode
+
+
+
+
+
 // -- SCORE & SPØRGSMÅL KNAP -- //
 $(".score_button").on("click", score_toggle);
 $(".question_button").on("click", question_toggle);
@@ -231,26 +528,4 @@ function question_answer_open() {
     } else {
         console.log("gør ingenting da question answer er åben")
     }
-}
-
-// -- DIRECTIONS / RUTEVEJLEDNING -- //
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-
-    directionsService.route({
-        origin: list[0].position,
-        destination: list[1].position,
-
-
-        optimizeWaypoints: true,
-        travelMode: 'WALKING'
-    }, function (response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-            //  var route = response.routes[0];
-
-        } else {
-            window.alert('Directions request failed due to ' + status);
-        }
-    });
 }
